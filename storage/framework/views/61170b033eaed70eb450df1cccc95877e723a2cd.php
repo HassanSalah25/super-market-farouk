c37440c8c95a25d68c79acdcc523fef7d4a89748
HassanSalah25 <salemhassan175@gmail.com>
2025-02-22 03:17:39 +0200

    aaa

---
+++ storage/framework/views/61170b033eaed70eb450df1cccc95877e723a2cd.php
@@ -0,0 +1,4 @@
+<a class="btn btn-primary btn-xs mb-3 mr-1" href="<?php echo e($url); ?>">
+    <i class="ti-pencil"></i>
+</a>
+<?php /**PATH C:\Users\Administrator\Local Sites\el-farouk-ecommerce\app\public\@core\resources\views/components/edit-icon.blade.php ENDPATH**/ ?>
